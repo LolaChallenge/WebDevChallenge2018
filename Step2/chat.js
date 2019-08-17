60a2f5d38dc6b6f3e068ccf516c5d146dfe56597
lawrayette <milhadasdepapel@gmail.com>
2019-08-17 17:07:03 -0500

    update chat.js

--- Step2/chat.js
+++ Step2/chat.js
@@ -1,101 +1,86 @@
-<script type="text/javascript">
-  var url = window.location.href;
-  var locale = url.split("/")[5];
-  var onlineGreeting, offlineGreeting;
-  if(locale === "es") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Habla con nosotras";
-    offlineGreeting = 'Deja un mensaje';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  } 
-  else if(locale === "pt-pt") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Fale com a gente";
-    offlineGreeting = 'Deixe uma mensagem';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  } 
-  else if(locale === "fr") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Parlez-nous";
-    offlineGreeting = 'Laissez un message';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  } 
-   else if(locale === "hi") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "हमारे साथ चैट करें";
-    offlineGreeting = "हमें एक संदेश छोड़ दो";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  }
-   else if(locale === "pl") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Rozmawiaj z nami";
-    offlineGreeting = "Zostaw nam wiadomość";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  }
-  else {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = "Leave us a message";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  }
-</script>
+// variables
+const url = window.location.href
+let locale = url.split('/')[5]
+const onlineGreeting, offlineGreeting
+
+//Function as a filter that control the lenguages in the web app depending his position on the window.location
+$zopim (function() {
+  $zopim.livechat.departments.filter('es', 'pt-pt', 'fr','hi','pl');
+})
+
+// I'm extract the duplicate code and I make a function that contain this code.
+const getZopim = () => {
+  window.$zopim || (function (d, s) {
+    var z = $zopim = function (c) {
+      z._.push(c)
+    }; var $ = z.s =
+    d.createElement(s); var e = d.getElementsByTagName(s)[0]; z.set = function (o) {
+      z.set
+        ._.push(o)
+    }; z._ = []; z.set._ = []; $.async = !0; $.setAttribute('charset', 'utf-8')
+    $.src = '//v2.zopim.com/?'; z.t = +new Date(); $
+      .type = 'text/javascript'; e.parentNode.insertBefore($, e)
+  })(document, 'script')
+}
+
+if (locale === 'es') {
+  getZopim()
+  onlineGreeting = 'Habla con nosotras'
+  offlineGreeting = 'Nuestros asesores no están disponibles en este momento, deje un mensaje y nos pondremos en contacto con usted pronto'
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
+      online: onlineGreeting,
+      offline: offlineGreeting
+    })
+  })
+} else if (locale === 'pt-pt') {
+  getZopim()
+  onlineGreeting = 'Fale com a gente'
+  offlineGreeting = 'Nossos conselheiros não estão disponíveis no momento, por favor deixe uma mensagem e entraremos em contato em breve'
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
+      online: onlineGreeting,
+      offline: offlineGreeting
+    })
+  })
+} else if (locale === 'fr') {
+  getZopim()
+  onlineGreeting = 'Parlez-nous'
+  offlineGreeting = 'Nos conseillers ne sont pas disponibles pour le moment, veuillez laisser un message et nous vous contacterons bientôt'
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
+      online: onlineGreeting,
+      offline: offlineGreeting
+    })
+  })
+} else if (locale === 'hi') {
+  getZopim()
+  onlineGreeting = 'हमारे साथ चैट करें'
+  offlineGreeting = ' हमारे काउंसलर फिलहाल उपलब्ध नहीं हैं, कृपया एक संदेश छोड़ें और हम आपसे जल्द ही संपर्क करेंगे'
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
+      online: onlineGreeting,
+      offline: offlineGreeting
+    })
+  })
+} else if (locale === 'pl') {
+  getZopim()
+  onlineGreeting = 'Rozmawiaj z nami'
+  offlineGreeting = 'Nasi doradcy nie są w tej chwili dostępni, proszę zostaw wiadomość, a my skontaktujemy się z Tobą wkrótce'
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
+      online: onlineGreeting,
+      offline: offlineGreeting
+    })
+  })
+} else {
+  getZopim()
+  onlineGreeting = 'Chat with us'
+  offlineGreeting = 'Our counselors are not available at the moment, please leave a message and we will contact you soon'
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
+      online: onlineGreeting,
+      offline: offlineGreeting
+    })
+  })
+}
